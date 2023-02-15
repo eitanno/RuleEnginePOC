ded9086545a34a222807526215384ba6e1a662ac
zidonb <zidonb@gmail.com>
2023-02-15 18:51:16 +0200

    refactor main engine to sub funcs

--- RE.Engine/MainEngine.cs
+++ RE.Engine/MainEngine.cs
@@ -6,7 +6,34 @@ using RulesEngine.Models;
 namespace RE.Engine;
 public class MainEngine
 {
+    private RulesEngine.RulesEngine taxRateEngine;
+
+    public MainEngine()
+    {
+        var jsonWorkflows = File.ReadAllText(Path.Combine("..", "assets", "MasRechishaFlows2023.json"));
+        RulesEngine.Models.Workflow[] workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
+        taxRateEngine = new RulesEngine.RulesEngine(workflows);
+    }
+
     async public Task<double> run(ExpandoObject input)
+    {
+        Console.WriteLine("Starting tax rate rules");
+        double taxRate = 0;
+
+        List<RuleResultTree> taxResponse = await runWorkflow("TaxRate", input);
+        taxRate = printInputRulesResult(taxResponse);
+
+        List<RuleResultTree> discountResponse = await runWorkflow("Discount", input);
+        taxRate = taxRate - printInputRulesResult(discountResponse);
+
+        List<RuleResultTree> specialFixRateResponse = await runWorkflow("SpecialFixRate", input);
+        taxRate = taxRate - printInputRulesResult(specialFixRateResponse);
+
+        return taxRate;
+    }
+
+
+    async public Task<List<RuleResultTree>> runWorkflow(string workflowName, ExpandoObject input)
     {
         Console.WriteLine("Starting tax rate rules");
 
@@ -15,21 +42,11 @@ public class MainEngine
 
         var taxRateEngine = new RulesEngine.RulesEngine(workflows);
 
-        double taxRate = 0;
+        return await taxRateEngine.ExecuteAllRulesAsync(workflowName, input);
 
 
-        List<RuleResultTree> taxResponse = await taxRateEngine.ExecuteAllRulesAsync("TaxRate", input);
-        taxRate = printInputRulesResult(taxResponse);
 
-        List<RuleResultTree> discountResponse = await taxRateEngine.ExecuteAllRulesAsync("Discount", input);
-        taxRate = taxRate - printInputRulesResult(discountResponse);
-
-        List<RuleResultTree> specialFixRateResponse = await taxRateEngine.ExecuteAllRulesAsync("SpecialFixRate", input);
-        taxRate = taxRate - printInputRulesResult(specialFixRateResponse);
-
-        return taxRate;
     }
-
     private double printInputRulesResult(List<RuleResultTree> response)
     {
         // TODO: if more then 1
