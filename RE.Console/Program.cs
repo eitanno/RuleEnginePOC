7d559e81fa66fcc88fe1fb3aaf829c51a7070e43
zidonb <zidonb@gmail.com>
2023-02-15 18:39:44 +0200

    seperate core code to library

--- RE.Console/Program.cs
+++ RE.Console/Program.cs
@@ -1,50 +1,16 @@
-﻿using System.ComponentModel;
-using System.Dynamic;
+﻿using System.Dynamic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using RulesEngine;
-using RulesEngine.Models;
-using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
+using RE.Engine;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Starting tax rate rules");
 
-var jsonWorkflows = File.ReadAllText(Path.Combine("MasRechishaFlows2023.json"));
-RulesEngine.Models.Workflow[] workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
-
-string jsonInput = System.IO.File.ReadAllText(@"MasRechishaInput.json");
+string jsonInput = System.IO.File.ReadAllText(Path.Combine("..", "assets", @"MasRechishaInput.json"));
 List<ExpandoObject> inputs = JsonConvert.DeserializeObject<List<ExpandoObject>>(jsonInput, new ExpandoObjectConverter());
 
-var taxRateEngine = new RulesEngine.RulesEngine(workflows);
-foreach(Object input in inputs) {
-    double taxRate = 0;
 
-    
-    List<RuleResultTree> taxResponse = await taxRateEngine.ExecuteAllRulesAsync("TaxRate", input);          
-    taxRate = printInputRulesResult(taxResponse);
-    
-    List<RuleResultTree> discountResponse = await taxRateEngine.ExecuteAllRulesAsync("Discount", input);
-    taxRate = taxRate - printInputRulesResult(discountResponse);
-
-    List<RuleResultTree> specialFixRateResponse = await taxRateEngine.ExecuteAllRulesAsync("SpecialFixRate", input);
-    taxRate = taxRate - printInputRulesResult(specialFixRateResponse);
+MainEngine engine = new MainEngine();
+if (inputs != null)
+{
+    inputs.ForEach(async inp => Console.WriteLine(await engine.run(inp)));
 }
-
-
-double printInputRulesResult(List<RuleResultTree> response) {
-    // TODO: if more then 1
-    return response
-        .Where(r => r.IsSuccess == true)
-        .Select(r => Convert.ToDouble(r.Rule.SuccessEvent))
-        .FirstOrDefault();
-    /*double taxRate = 0;
-    foreach(var ruleResult in response){   
-        if(ruleResult.IsSuccess){
-            string sucessEvent = ruleResult.Rule.SuccessEvent;
-            Console.WriteLine(sucessEvent);
-           taxRate =  Convert.ToDouble(sucessEvent);
-        }
-    }
-    return taxRate;*/
-}
+Console.ReadLine();
---
+++ RE.Engine/MainEngine.cs
@@ -0,0 +1,50 @@
+﻿using System.Dynamic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RulesEngine.Models;
+
+namespace RE.Engine;
+public class MainEngine
+{
+    async public Task<double> run(ExpandoObject input)
+    {
+        Console.WriteLine("Starting tax rate rules");
+
+        var jsonWorkflows = File.ReadAllText(Path.Combine("..", "assets", "MasRechishaFlows2023.json"));
+        RulesEngine.Models.Workflow[] workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
+
+        var taxRateEngine = new RulesEngine.RulesEngine(workflows);
+
+        double taxRate = 0;
+
+
+        List<RuleResultTree> taxResponse = await taxRateEngine.ExecuteAllRulesAsync("TaxRate", input);
+        taxRate = printInputRulesResult(taxResponse);
+
+        List<RuleResultTree> discountResponse = await taxRateEngine.ExecuteAllRulesAsync("Discount", input);
+        taxRate = taxRate - printInputRulesResult(discountResponse);
+
+        List<RuleResultTree> specialFixRateResponse = await taxRateEngine.ExecuteAllRulesAsync("SpecialFixRate", input);
+        taxRate = taxRate - printInputRulesResult(specialFixRateResponse);
+
+        return taxRate;
+    }
+
+    private double printInputRulesResult(List<RuleResultTree> response)
+    {
+        // TODO: if more then 1
+        return response
+            .Where(r => r.IsSuccess == true)
+            .Select(r => Convert.ToDouble(r.Rule.SuccessEvent))
+            .FirstOrDefault();
+        /*double taxRate = 0;
+        foreach(var ruleResult in response){   
+            if(ruleResult.IsSuccess){
+                string sucessEvent = ruleResult.Rule.SuccessEvent;
+                Console.WriteLine(sucessEvent);
+            taxRate =  Convert.ToDouble(sucessEvent);
+            }
+        }
+        return taxRate;*/
+    }
+}
