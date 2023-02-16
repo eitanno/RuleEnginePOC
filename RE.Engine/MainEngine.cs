2763785163a8892a65e51b49b84f874917aa357c
zidonb <zidonb@gmail.com>
2023-02-16 08:18:17 +0200

    Cleaning code

--- RE.Engine/MainEngine.cs
+++ RE.Engine/MainEngine.cs
@@ -6,13 +6,13 @@ using RulesEngine.Models;
 namespace RE.Engine;
 public class MainEngine
 {
-    private string jsonWorkflows;
+    private RulesEngine.Models.Workflow[] workflows;
     private RulesEngine.RulesEngine taxRateEngine;
 
     public MainEngine(string flowsFileName)
     {
-        jsonWorkflows = File.ReadAllText(Path.Combine("..", "assets", flowsFileName));
-        RulesEngine.Models.Workflow[] workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
+        string jsonWorkflows = File.ReadAllText(Path.Combine("..", "assets", flowsFileName));
+        workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
         taxRateEngine = new RulesEngine.RulesEngine(workflows);
     }
 
@@ -20,42 +20,25 @@ public class MainEngine
     {
         double taxRate = 0;
 
-        List<RuleResultTree> taxResponse = await runWorkflow("TaxRate", input);
-        taxRate = printInputRulesResult(taxResponse);
+        List<RuleResultTree> taxResponse = await taxRateEngine.ExecuteAllRulesAsync("TaxRate", input);
+        taxRate = getFirstSuccessRule(taxResponse);
 
-        List<RuleResultTree> discountResponse = await runWorkflow("Discount", input);
-        taxRate = taxRate - printInputRulesResult(discountResponse);
+        List<RuleResultTree> discountResponse = await taxRateEngine.ExecuteAllRulesAsync("Discount", input);
+        taxRate = taxRate - getFirstSuccessRule(discountResponse);
 
-        List<RuleResultTree> specialFixRateResponse = await runWorkflow("SpecialFixRate", input);
-        taxRate = taxRate - printInputRulesResult(specialFixRateResponse);
+        List<RuleResultTree> specialFixRateResponse = await taxRateEngine.ExecuteAllRulesAsync("SpecialFixRate", input);
+        taxRate = taxRate - getFirstSuccessRule(specialFixRateResponse);
 
         return taxRate;
     }
 
 
-    async public Task<List<RuleResultTree>> runWorkflow(string workflowName, ExpandoObject input)
-    {
-        RulesEngine.Models.Workflow[] workflows = JsonConvert.DeserializeObject<RulesEngine.Models.Workflow[]>(jsonWorkflows);
-
-        var taxRateEngine = new RulesEngine.RulesEngine(workflows);
-
-        return await taxRateEngine.ExecuteAllRulesAsync(workflowName, input);
-    }
-    private double printInputRulesResult(List<RuleResultTree> response)
+    private double getFirstSuccessRule(List<RuleResultTree> response)
     {
         // TODO: if more then 1
         return response
             .Where(r => r.IsSuccess == true)
             .Select(r => Convert.ToDouble(r.Rule.SuccessEvent))
             .FirstOrDefault();
-        /*double taxRate = 0;
-        foreach(var ruleResult in response){   
-            if(ruleResult.IsSuccess){
-                string sucessEvent = ruleResult.Rule.SuccessEvent;
-                Console.WriteLine(sucessEvent);
-            taxRate =  Convert.ToDouble(sucessEvent);
-            }
-        }
-        return taxRate;*/
     }
 }
