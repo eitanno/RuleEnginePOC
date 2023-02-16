ea20520f57893ab3e871c1bdb96e1468bb64e010
zidonb <zidonb@gmail.com>
2023-02-16 07:42:40 +0200

    Printing input name to console for testing

--- RE.Console/Program.cs
+++ RE.Console/Program.cs
@@ -7,10 +7,9 @@ Console.WriteLine("Start evaluating Mas Rechisha for various inputs - ");
 string jsonInput = System.IO.File.ReadAllText(Path.Combine("..", "assets", @"MasRechishaInput.json"));
 List<ExpandoObject> inputs = JsonConvert.DeserializeObject<List<ExpandoObject>>(jsonInput, new ExpandoObjectConverter());
 
-
 MainEngine engine = new MainEngine("MasRechishaFlows2023.json");
 if (inputs != null)
 {
-    inputs.ForEach(async inp => Console.WriteLine(await engine.run(inp)));
+    inputs.ForEach(async inp => Console.WriteLine(inp.FirstOrDefault(x => x.Key == "name").Value + " taxRate is " + await engine.run(inp)));
 }
 Console.ReadLine();
