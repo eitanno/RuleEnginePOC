52cdd24be43e24e47e21811ed484b37a78df46f2
zidonb <zidonb@gmail.com>
2023-02-21 10:43:10 +0200

    Clean

--- RE.Engine/MasRechishaEngine.cs
+++ RE.Engine/MasRechishaEngine.cs
@@ -14,44 +14,55 @@ public class MasRechishaEngine : GenericEngine
 
     async public Task<TaxResult> run(ExpandoObject input)
     {
+        RuleResultTree? result;
         TaxResult tax = new TaxResult();
 
         List<RuleResultTree> taxResponse = await microsoftRuleEngine.ExecuteAllRulesAsync("TaxRate", input);
-        resolveTaxFromRules(taxResponse, tax);
-
-        RuleResultTree? rule = getRule(taxResponse);
-        if (rule != null && rule.Rule.Actions != null && rule.Rule.Actions.OnSuccess != null && rule.Rule.Actions.OnSuccess.Context["Brackets"] != null)
-        {
-            string s = "";
-            var b = rule.Rule.Actions.OnSuccess.Context["Brackets"].ToString().Split(',').ToList();
-            var p = rule.Rule.Actions.OnSuccess.Context["Percentage"].ToString().Split(',').ToList();
-            var bt = rule.Rule.Actions.OnSuccess.Context["BracketsTax"].ToString().Split(',').ToList();
-            var size = b.Count;
-            bt[size - 1] = rule.ActionResult.Output.ToString();
-            Double l = 0;
-            for (int i = 0; i < size; i++)
-            {
-                tax.addBracketsTax(i, b[i], p[i], bt[i]);
-                s = s + "Index: " + i + " Till: " + b[i] + " Percentage: " + p[i] + " Should pay: " + bt[i] + ";";
-                l = l + Convert.ToDouble(bt[i]);
-            }
-
-            s = s + " Sum: " + l;
-            tax.sumOfBackets = l;
-            tax.setBracketsTaxRate(s);
-        }
+        result = resolveTaxFromRules(taxResponse, tax);
+        
 
         List<RuleResultTree> discountResponse = await microsoftRuleEngine.ExecuteAllRulesAsync("Discount", input);
-        resolveTaxFromRules(discountResponse, tax);
+        if (hasSuccesRule(discountResponse))
+        {
+            result = resolveTaxFromRules(discountResponse, tax);
+        }
 
         List<RuleResultTree> specialFixRateResponse = await microsoftRuleEngine.ExecuteAllRulesAsync("SpecialFixRate", input);
         if (hasSuccesRule(specialFixRateResponse))
         {
-            resolveTaxFromRules(specialFixRateResponse, tax);
+            result = resolveTaxFromRules(specialFixRateResponse, tax);
         }
+
+        presentTaxTable(tax, result);
+        
         Console.WriteLine(input.FirstOrDefault(x => x.Key == "name").Value +
         " taxRate is " + tax.getTaxRate() + ",      from rule? :" + tax.IsResolvedFromRule());
         return tax;
+    }
+
+    private void presentTaxTable(TaxResult tax, RuleResultTree? result)
+    {
+        if (result != null && result.Rule.Actions != null && result.Rule.Actions.OnSuccess != null && result.Rule.Actions.OnSuccess.Context["Brackets"] != null)
+        {
+            
+            string s = "";
+            var purchaseBrackets = result.Rule.Actions.OnSuccess.Context["Brackets"].ToString().Split(',').ToList();
+            var percentageBrackets = result.Rule.Actions.OnSuccess.Context["Percentage"].ToString().Split(',').ToList();
+            var taxSumBrackets = result.Rule.Actions.OnSuccess.Context["BracketsTax"].ToString().Split(',').ToList();
+            var size = purchaseBrackets.Count;
+            taxSumBrackets[size - 1] = result.ActionResult.Output.ToString();
+            Double taxToPay = 0;
+            for (int i = 0; i < size; i++)
+            {
+                tax.addBracketsTax(i, purchaseBrackets[i], percentageBrackets[i], taxSumBrackets[i]);
+                s = s + "Index: " + i + " Till: " + purchaseBrackets[i] + " Percentage: " + percentageBrackets[i] + " Should pay: " + taxSumBrackets[i] + ";";
+                taxToPay = taxToPay + Convert.ToDouble(taxSumBrackets[i]);
+            }
+
+            s = s + " Sum: " + taxToPay;
+            tax.sumOfBackets = taxToPay;
+            tax.setBracketsTaxRate(s);
+        }
     }
 
     // The workflow file name
